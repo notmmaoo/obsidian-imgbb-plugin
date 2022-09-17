e519dea9c0522c2ddee1a5207451acf50d923bdc
hufeng.mao <hufeng.mao@carota.ai>
2022-09-17 18:38:01 +0800

    feat: 初步实现上传图片到ImgBB的功能

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, requestUrl } from 'obsidian';
 
+import { resolve, relative, join, parse, posix, extname } from "path";
+import { existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
@@ -9,6 +11,17 @@ interface MyPluginSettings {
 const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
+
+interface Image {
+	path: string;
+	name: string;
+	source: string;
+}
+
+const REGEX_FILE = /\!\[(.*?)\]\((.*?)\)/g;
+const REGEX_WIKI_FILE = /\!\[\[(.*?)\]\]/g;
+
+const IMAGE_TYPE = [".png", ".jpg", ".jpeg", ".bmp", ".gif", ".svg", ".tiff"]
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -65,6 +78,14 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
+		this.addCommand({
+			id: 'upload all images',
+			name: 'Upload all images',
+			callback: () => {
+				this.uploadAllImages();
+			}
+		});
+
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
@@ -88,6 +109,94 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	uploadAllImages() {
+		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (markdownView) {
+			const editor = markdownView.editor;
+			const doc = editor.getDoc();
+			const value = doc.getValue();
+			const matches = value.matchAll(REGEX_FILE);
+			const WikiMatches = value.matchAll(REGEX_WIKI_FILE);
+
+			let imageList: Image[] = [];
+
+			for (const match of matches) {
+				const name = match[1];
+				const path = match[2].toLowerCase();
+				const source = match[0];
+
+				let ext = extname(path)
+				if (path.startsWith('http') || !IMAGE_TYPE.includes(ext)) {
+					continue
+				}
+				if (existsSync(path)) {
+					imageList.push({
+						path: path,
+						name: name,
+						source: source,
+					});
+				}
+			}
+
+			for (const match of WikiMatches) {
+				const name = parse(match[1]).name;
+				const path = match[1].toLowerCase();;
+				const source = match[0];
+
+				let ext = extname(path)
+				if (path.startsWith('http') || !IMAGE_TYPE.includes(ext)) {
+					continue
+				}
+				if (existsSync(path)) {
+					imageList.push({
+						path: path,
+						name: name,
+						source: source,
+					});
+				}
+			}
+			
+			console.log(imageList);
+			if (imageList.length === 0) {
+				new Notice("没有解析到图像文件");
+			return;
+			} else {
+				new Notice(`共找到${imageList.length}个图像文件，开始上传`);
+			}
+
+			// return imageList;
+			this.uploadImage(imageList);
+		}
+	}
+
+	async uploadFile(localFile: string, name: string): Promise<any> {
+		// post base64 of file
+		const file = readFileSync(localFile);
+		const base64 = file.toString('base64');
+		const form = new FormData();
+		form.append('key', this.settings.mySetting);
+		form.append('name', name);
+		form.append('image', base64);
+
+		const response = await fetch('https://api.imgbb.com/1/upload', {
+		  method: "POST",
+		  body: form,
+		});
+	
+		const data = response.json();
+		console.log(data)
+		return data;
+	}
+	uploadImage(imageList: Image[]) {
+		
+		for (const match of imageList) {
+			const name = match.name;
+			const localFile = match.path;
+			this.uploadFile(localFile, name);
+		}
+	  
 	}
 }
 
@@ -123,7 +232,7 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
+			.setName('Setting KEY')
 			.setDesc('It\'s a secret')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
