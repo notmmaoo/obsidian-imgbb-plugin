2d20ae822b7cb836faac19abdc064e410e8f0966
hufeng.mao <hufeng.mao@carota.ai>
2022-09-18 15:40:11 +0800

    feat: 上传图片成功后替换本地连接
    - 清理了一些模板代码

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, requestUrl } from 'obsidian';
+import { App, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-import { resolve, relative, join, parse, posix, extname } from "path";
-import { existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
+import { parse, extname } from "path";
+import { existsSync, readFileSync } from "fs";
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
@@ -30,9 +30,8 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Imgbb Plugin', (evt: MouseEvent) => {
+			this.uploadAllImages();
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -40,43 +39,6 @@ export default class MyPlugin extends Plugin {
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		this.addCommand({
 			id: 'upload all images',
@@ -88,15 +50,6 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -115,8 +68,7 @@ export default class MyPlugin extends Plugin {
 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 		if (markdownView) {
 			const editor = markdownView.editor;
-			const doc = editor.getDoc();
-			const value = doc.getValue();
+			let value = editor.getValue();
 			const matches = value.matchAll(REGEX_FILE);
 			const WikiMatches = value.matchAll(REGEX_WIKI_FILE);
 
@@ -157,17 +109,32 @@ export default class MyPlugin extends Plugin {
 					});
 				}
 			}
-			
+
 			console.log(imageList);
 			if (imageList.length === 0) {
 				new Notice("没有解析到图像文件");
-			return;
+				return;
 			} else {
 				new Notice(`共找到${imageList.length}个图像文件，开始上传`);
 			}
 
-			// return imageList;
-			this.uploadImage(imageList);
+			// upload imageList;
+			for (const match of imageList) {
+				const name = match.name;
+				const localFile = match.path;
+				this.uploadFile(localFile, name).then(res => {
+					if (res.success) {
+						let uploadUrlList = res.data.display_url;
+						value = value.replace(match.source, `![${name}](${uploadUrlList})`);
+						new Notice(uploadUrlList);
+					} else {
+						new Notice("Upload error");
+					}
+				}).then(() => {
+					console.log("update editor");
+					editor.setValue(value);
+				});
+			}
 		}
 	}
 
@@ -181,38 +148,13 @@ export default class MyPlugin extends Plugin {
 		form.append('image', base64);
 
 		const response = await fetch('https://api.imgbb.com/1/upload', {
-		  method: "POST",
-		  body: form,
+			method: "POST",
+			body: form,
 		});
-	
+
 		const data = response.json();
 		console.log(data)
 		return data;
-	}
-	uploadImage(imageList: Image[]) {
-		
-		for (const match of imageList) {
-			const name = match.name;
-			const localFile = match.path;
-			this.uploadFile(localFile, name);
-		}
-	  
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -225,11 +167,11 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
 
 		new Setting(containerEl)
 			.setName('Setting KEY')
